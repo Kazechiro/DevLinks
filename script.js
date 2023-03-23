b69e7cdf747a1bbf3ec250d028cb8e9ad60e037f
Kaio Barbosa <kaiob419@gmail.com>
2023-03-23 13:01:32 -0300

    fixing bg error

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ html.classList.toggle('light');
   if(html.classList.contains('light')) {
       img.setAttribute('src', './assets/avatar-light.png')
    } else{
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute('src','./assets/avatar.png')
  
     }
 
